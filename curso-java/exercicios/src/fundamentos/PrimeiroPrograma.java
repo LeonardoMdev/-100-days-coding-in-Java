9adacfac484fe71a3dc0702150f4317d93c5875a
Leonardo Moreira <97768128+LeonardoMdev@users.noreply.github.com>
2024-12-02 19:49:23 -0300

    Add files via upload
    first commit , start of the project

---
+++ curso-java/exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+package fundamentos;
+
+public class PrimeiroPrograma {
+	public static void main(String[] args) {
+		// Uma sentença de código termina com ; 
+		System.out.println("Primeiro programa , Hello World");
+		/*
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * */
+	}
+}
---
+++ curso-java/faculdade/src/tesouraria/Mensalidades.java
@@ -0,0 +1,5 @@
+package tesouraria;
+
+public class Mensalidades {
+
+}
